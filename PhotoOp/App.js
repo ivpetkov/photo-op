1ee20c4c9703129b37645f0bac4c9de4a2349951
Iva Petkov <ivpetkov@ucsc.edu>
2019-11-20 14:10:23 -0800

    working basic navigation between list and details page

--- PhotoOp/App.js
+++ PhotoOp/App.js
@@ -19,17 +19,46 @@ export default App
 */
 
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, StyleSheet } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
         <Button
-          title="Go to Details"
+          title="Location 1"
+          onPress={() => this.props.navigation.navigate('Details1')}
+        />
+        <Button
+          title="Location 2"
+          onPress={() => this.props.navigation.navigate('Details2')}
+        />
+        <Button
+          title="Location 3"
+          onPress={() => this.props.navigation.navigate('Details3')}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen1 extends React.Component {
+  static navigationOptions = {
+    title: 'Location 1 Details',
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
           onPress={() => this.props.navigation.navigate('Details')}
         />
       </View>
@@ -37,7 +66,29 @@ class HomeScreen extends React.Component {
   }
 }
 
-class DetailsScreen extends React.Component {
+class DetailsScreen2 extends React.Component {
+  static navigationOptions = {
+    title: 'Location 2 Details',
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen3 extends React.Component {
+  static navigationOptions = {
+    title: 'Location 3 Details',
+  };
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -54,10 +105,23 @@ class DetailsScreen extends React.Component {
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen,
+    Details1: DetailsScreen1,
+    Details2: DetailsScreen2,
+    Details3: DetailsScreen3,
   },
   {
     initialRouteName: 'Home',
+
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f765b1',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        fontSize: 16,
+      },
+    },
   }
 );
 
