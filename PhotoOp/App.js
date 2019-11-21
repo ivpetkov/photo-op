37f54f2db28ad62e42e2e4edf5df8abe54555083
Iva Petkov <ivpetkov@ucsc.edu>
2019-11-21 11:45:36 -0800

    more details screens

--- PhotoOp/App.js
+++ PhotoOp/App.js
@@ -1,23 +1,3 @@
-/*
-import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
-import firebase from 'react-native-firebase';
-
-// home page before navigation
-import List from './screens/List.js'
-import Header from './screens/Header.js'
-
-const App = () => {
-   return (
-     <ScrollView>
-        <Header />
-        <List />
-      </ScrollView>
-   )
-}
-export default App
-*/
-
 import * as React from 'react';
 import { Button, View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 import { createAppContainer } from 'react-navigation';
@@ -44,9 +24,26 @@ class HomeScreen extends React.Component {
           title="Location 3"
           onPress={() => this.props.navigation.navigate('Details3')}
         />
-      </View>
+        <Button
+          title="Location 4"
+          onPress={() => this.props.navigation.navigate('Details4')}
+        />
+        <Button
+          title="Location 5"
+          onPress={() => this.props.navigation.navigate('Details5')}
+        />
+        <Button
+          title="Location 6"
+          onPress={() => this.props.navigation.navigate('Details6')}
+        />
+        <Button
+          title="Location 7"
+          onPress={() => this.props.navigation.navigate('Details7')}
+        />
+    </View>
     );
   }
+
 }
 
 class DetailsScreen1 extends React.Component {
@@ -118,12 +115,108 @@ class DetailsScreen3 extends React.Component {
   }
 }
 
+class DetailsScreen4 extends React.Component {
+  static navigationOptions = {
+    title: 'Location 4 Details',
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center'}}>
+        <ScrollView>
+          <Text>Name of recommended photo location: UCSC Arboretum</Text>
+          <Text>Photo of recommended photo location</Text>
+          <Image
+            style={{width: 300, height: 200}}
+            source={require('./images/arboretum.jpg')}
+          />
+          <Text>Description of recommended photo location</Text>
+          <Text>Address of recommended photo location</Text>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+class DetailsScreen5 extends React.Component {
+  static navigationOptions = {
+    title: 'Location 5 Details',
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center'}}>
+        <ScrollView>
+          <Text>Name of recommended photo location: Seabright State Beach</Text>
+          <Text>Photo of recommended photo location</Text>
+          <Image
+            style={{width: 300, height: 200}}
+            source={require('./images/seabright.jpg')}
+          />
+          <Text>Description of recommended photo location</Text>
+          <Text>Address of recommended photo location</Text>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+class DetailsScreen6 extends React.Component {
+  static navigationOptions = {
+    title: 'Location 6 Details',
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center'}}>
+        <ScrollView>
+          <Text>Name of recommended photo location: West Cliff</Text>
+          <Text>Photo of recommended photo location</Text>
+          <Image
+            style={{width: 300, height: 200}}
+            source={require('./images/west_cliff.jpg')}
+          />
+          <Text>Description of recommended photo location</Text>
+          <Text>Address of recommended photo location</Text>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+class DetailsScreen7 extends React.Component {
+  static navigationOptions = {
+    title: 'Location 7 Details',
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center'}}>
+        <ScrollView>
+          <Text>Name of recommended photo location: Wilder Ranch State Park</Text>
+          <Text>Photo of recommended photo location</Text>
+          <Image
+            style={{width: 300, height: 200}}
+            source={require('./images/wilder.jpg')}
+          />
+          <Text>Description of recommended photo location</Text>
+          <Text>Address of recommended photo location</Text>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
     Details1: DetailsScreen1,
     Details2: DetailsScreen2,
     Details3: DetailsScreen3,
+    Details4: DetailsScreen4,
+    Details5: DetailsScreen5,
+    Details6: DetailsScreen6,
+    Details7: DetailsScreen7,
   },
   {
     initialRouteName: 'Home',
