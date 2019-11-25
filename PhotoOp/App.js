8eddc02cae946ea99b93c4d51c45922ad2332b73
Iva Petkov <ivpetkov@ucsc.edu>
2019-11-24 23:37:38 -0800

    geocoding working with navigation

--- PhotoOp/App.js
+++ PhotoOp/App.js
@@ -1,77 +1,216 @@
 import * as React from 'react';
-import { Button, View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { Button, View, Text, StyleSheet, Image, ScrollView, TextInput, ActivityIndicator } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { getDistance, convertDistance } from 'geolib';
+//import LocationButtons from './src/LocationButtons.js'
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Home',
   };
 
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center' }}>
-        <Text>Current Location: Santa Cruz, CA </Text>
-        <Button
-          title="Location 1"
-          onPress={() => this.props.navigation.navigate('Details1')}
-        />
-        <Button
-          title="Location 2"
-          onPress={() => this.props.navigation.navigate('Details2')}
-        />
-        <Button
-          title="Location 3"
-          onPress={() => this.props.navigation.navigate('Details3')}
-        />
-        <Button
-          title="Location 4"
-          onPress={() => this.props.navigation.navigate('Details4')}
-        />
-        <Button
-          title="Location 5"
-          onPress={() => this.props.navigation.navigate('Details5')}
-        />
-        <Button
-          title="Location 6"
-          onPress={() => this.props.navigation.navigate('Details6')}
-        />
-        <Button
-          title="Location 7"
-          onPress={() => this.props.navigation.navigate('Details7')}
-        />
-    </View>
-    );
+  constructor(props){
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      dist: 0,
+      photoDists: [],
+      distVals:[],
+      isLoading: true,
+    }
+    this.calculateDistance = this.calculateDistance.bind(this);
   }
 
+  async componentDidMount(){
+    await this.calculateDistance();
+  }
+
+  async getPhotoCoords(){
+    await fetch('https://maps.googleapis.com/maps/api/geocode/json?address=2531+W+Cliff+Dr,+Santa+Cruz,+CA&key=AIzaSyBv__05nyUa8JC7A1WRZ4KCDJnfYP5Bt5o')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      var location = "Natural Bridges";
+      var latitude = responseJson.results[0].geometry.location.lat;
+      var longitude = responseJson.results[0].geometry.location.lng;
+      let photoDistsHistory = [...this.state.photoDists];
+      photoDistsHistory.push([location, latitude, longitude])
+      this.setState({
+        photoDists: photoDistsHistory
+      });
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+
+    await fetch('https://maps.googleapis.com/maps/api/geocode/json?address=30+Front+St,+Santa+Cruz,+CA&key=AIzaSyBv__05nyUa8JC7A1WRZ4KCDJnfYP5Bt5o')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      var location = "Boardwalk Mural";
+      var latitude = responseJson.results[0].geometry.location.lat;
+      var longitude = responseJson.results[0].geometry.location.lng;
+      let photoDistsHistory = [...this.state.photoDists];
+      photoDistsHistory.push([location, latitude, longitude])
+      this.setState({
+        photoDists: photoDistsHistory
+      });
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+
+    await fetch('https://maps.googleapis.com/maps/api/geocode/json?address=1200+Heller+Dr,+Santa+Cruz,+CA&key=AIzaSyBv__05nyUa8JC7A1WRZ4KCDJnfYP5Bt5o')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      var location = "Porter Squiggle";
+      var latitude = responseJson.results[0].geometry.location.lat;
+      var longitude = responseJson.results[0].geometry.location.lng;
+      let photoDistsHistory = [...this.state.photoDists];
+      photoDistsHistory.push([location, latitude, longitude])
+      this.setState({
+        photoDists: photoDistsHistory
+      });
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+
+    await fetch('https://maps.googleapis.com/maps/api/geocode/json?address=1104+E+Cliff+Dr,+Santa+Cruz,+CA&key=AIzaSyBv__05nyUa8JC7A1WRZ4KCDJnfYP5Bt5o')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      var location = "Seabright";
+      var latitude = responseJson.results[0].geometry.location.lat;
+      var longitude = responseJson.results[0].geometry.location.lng;
+      let photoDistsHistory = [...this.state.photoDists];
+      photoDistsHistory.push([location, latitude, longitude])
+      this.setState({
+        photoDists: photoDistsHistory
+      });
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+
+    //temporarily took out other locations to make file smaller
+
+  }
+
+  async calculateDistance(){
+    await this.getCoords();
+    await this.getPhotoCoords();
+
+    for(var i = 0; i < this.state.photoDists.length; i++){
+      calculated_dist = getDistance(
+        {latitude: this.state.latitude, longitude: this.state.longitude },
+        {latitude: this.state.photoDists[i][1], longitude: this.state.photoDists[i][2]}
+      );
+      calculated_dist = convertDistance(calculated_dist, 'mi');
+      let distValsHistory = [...this.state.distVals];
+      distValsHistory.push([this.state.photoDists[i][0], calculated_dist])
+      this.setState({
+        distVals: distValsHistory
+      });
+    }
+    await this.sortLocations();
+    await this.toArrayOfObjects();
+    this.setState({
+      isLoading: false
+    });
+  }
+
+  getCoords(){
+    var address = '400+Beach+St,';
+    var apiCall = 'https://maps.googleapis.com/maps/api/geocode/json?address='+address+'Santa+Cruz,+CA&key=AIzaSyBv__05nyUa8JC7A1WRZ4KCDJnfYP5Bt5o';
+    return fetch(apiCall)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        latitude: responseJson.results[0].geometry.location.lat,
+        longitude: responseJson.results[0].geometry.location.lng
+      });
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+  }
+
+  comparator(a,b){
+    if (a[1] < b[1]) return -1;
+    if (a[1] > b[1]) return 1;
+    return 0;
+  }
+
+  sortLocations(){
+    myArray = this.state.distVals.sort(this.comparator);
+    this.setState({
+      distVals: myArray
+    });
+  }
+
+  toArrayOfObjects(){
+    var distValsObjectsArray = [];
+    for(var i = 0; i < this.state.distVals.length; i++) {
+        var obj = {
+          key: i+1,
+          text: this.state.distVals[i][0] + " " + this.state.distVals[i][1].toFixed(1) + " mi",
+          name: this.state.distVals[i][0].substring(0,3),
+        }
+        distValsObjectsArray.push(obj);
+    }
+    this.setState({
+      distVals: distValsObjectsArray
+    });
+  }
+
+  render(){
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 50}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    const photoButtons = this.state.distVals.map(b => {
+      return <Button key={b.key} title={b.text} onPress={() => this.props.navigation.navigate(b.name)} />;
+    });
+
+    return(
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        <Text style={{ padding: 20}}>Current Location: Santa Cruz, CA </Text>
+        {photoButtons}
+      </View>
+    );
+  }
 }
 
-class DetailsScreen1 extends React.Component {
+class NaturalBridges extends React.Component {
   static navigationOptions = {
-    title: 'Location 1 Details',
+    title: 'Natural Bridges',
   };
 
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center'}}>
         <ScrollView>
-          <Text>Name of recommended photo location: Natural Bridges</Text>
-          <Text>Photo of recommended photo location</Text>
+          <Text style={{paddingBottom: 10}}>Name of recommended photo location: Natural Bridges</Text>
+          <Text style={{paddingBottom: 10}}>Photo of recommended photo location</Text>
           <Image
             style={{width: 300, height: 200}}
-            source={require('./images/natural_bridges.png')}
+            source={require('./src/images/natural_bridges.png')}
           />
-          <Text>Description of recommended photo location</Text>
-          <Text>Address of recommended photo location</Text>
+          <Text style={{paddingBottom: 10}}>Description of recommended photo location</Text>
+          <Text style={{paddingBottom: 10}}>Address of recommended photo location</Text>
         </ScrollView>
       </View>
     );
   }
 }
 
-class DetailsScreen2 extends React.Component {
+class PorterSquiggle extends React.Component {
   static navigationOptions = {
-    title: 'Location 2 Details',
+    title: 'Porter Squiggle',
   };
 
   render() {
@@ -82,7 +221,7 @@ class DetailsScreen2 extends React.Component {
           <Text>Photo of recommended photo location</Text>
           <Image
             style={{width: 300, height: 200}}
-            source={require('./images/squiggle.jpg')}
+            source={require('./src/images/squiggle.jpg')}
           />
           <Text>Description of recommended photo location</Text>
           <Text>Address of recommended photo location</Text>
@@ -92,9 +231,9 @@ class DetailsScreen2 extends React.Component {
   }
 }
 
-class DetailsScreen3 extends React.Component {
+class BoardwalkMural extends React.Component {
   static navigationOptions = {
-    title: 'Location 3 Details',
+    title: 'Boardwalk Mural',
   };
 
   render() {
@@ -105,7 +244,7 @@ class DetailsScreen3 extends React.Component {
           <Text>Photo of recommended photo location</Text>
           <Image
             style={{width: 300, height: 200}}
-            source={require('./images/mural.jpg')}
+            source={require('./src/images/mural.jpg')}
           />
           <Text>Description of recommended photo location</Text>
           <Text>Address of recommended photo location</Text>
@@ -115,9 +254,9 @@ class DetailsScreen3 extends React.Component {
   }
 }
 
-class DetailsScreen4 extends React.Component {
+class Seabright extends React.Component {
   static navigationOptions = {
-    title: 'Location 4 Details',
+    title: 'Seabright',
   };
 
   render() {
@@ -128,76 +267,7 @@ class DetailsScreen4 extends React.Component {
           <Text>Photo of recommended photo location</Text>
           <Image
             style={{width: 300, height: 200}}
-            source={require('./images/arboretum.jpg')}
-          />
-          <Text>Description of recommended photo location</Text>
-          <Text>Address of recommended photo location</Text>
-        </ScrollView>
-      </View>
-    );
-  }
-}
-
-class DetailsScreen5 extends React.Component {
-  static navigationOptions = {
-    title: 'Location 5 Details',
-  };
-
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center'}}>
-        <ScrollView>
-          <Text>Name of recommended photo location: Seabright State Beach</Text>
-          <Text>Photo of recommended photo location</Text>
-          <Image
-            style={{width: 300, height: 200}}
-            source={require('./images/seabright.jpg')}
-          />
-          <Text>Description of recommended photo location</Text>
-          <Text>Address of recommended photo location</Text>
-        </ScrollView>
-      </View>
-    );
-  }
-}
-
-class DetailsScreen6 extends React.Component {
-  static navigationOptions = {
-    title: 'Location 6 Details',
-  };
-
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center'}}>
-        <ScrollView>
-          <Text>Name of recommended photo location: West Cliff</Text>
-          <Text>Photo of recommended photo location</Text>
-          <Image
-            style={{width: 300, height: 200}}
-            source={require('./images/west_cliff.jpg')}
-          />
-          <Text>Description of recommended photo location</Text>
-          <Text>Address of recommended photo location</Text>
-        </ScrollView>
-      </View>
-    );
-  }
-}
-
-class DetailsScreen7 extends React.Component {
-  static navigationOptions = {
-    title: 'Location 7 Details',
-  };
-
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center'}}>
-        <ScrollView>
-          <Text>Name of recommended photo location: Wilder Ranch State Park</Text>
-          <Text>Photo of recommended photo location</Text>
-          <Image
-            style={{width: 300, height: 200}}
-            source={require('./images/wilder.jpg')}
+            source={require('./src/images/seabright.jpg')}
           />
           <Text>Description of recommended photo location</Text>
           <Text>Address of recommended photo location</Text>
@@ -210,13 +280,10 @@ class DetailsScreen7 extends React.Component {
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Details1: DetailsScreen1,
-    Details2: DetailsScreen2,
-    Details3: DetailsScreen3,
-    Details4: DetailsScreen4,
-    Details5: DetailsScreen5,
-    Details6: DetailsScreen6,
-    Details7: DetailsScreen7,
+    Por: PorterSquiggle,
+    Boa: BoardwalkMural,
+    Sea: Seabright,
+    Nat: NaturalBridges,
   },
   {
     initialRouteName: 'Home',
